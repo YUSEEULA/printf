5ccd8135da642f3e440ae361d2e0b5f0233e7e2e
YUSEEULA <yousraelh12@gmail.com>
2023-04-23 09:41:10 -0700

    functions1_created

---
+++ functions1.c
@@ -0,0 +1,104 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/**
+ * print_u - Function that prints an unsigned number
+ * @num: Number to be printed
+ * Return: Number of digits printed
+ */
+
+int print_u(unsigned int num)
+{
+	int digits = 0;
+	char buffer[10];
+
+	do {
+		buffer[digits++] = (num % 10) + '0';
+		num = num / 10;
+	} while (num != 0);
+
+	while (digits-- > 0)
+	{
+		if (write(1, &buffer[digits], 1) == -1)
+			return (-1);
+	}
+
+	return (digits + 1);
+}
+
+/**
+ * print_o - Function that prints a number in octal
+ * @num: Number to be printed
+ * Return: Number of digits printed
+ */
+
+int print_o(unsigned int num)
+{
+	int digits = 0;
+	char buffer[22];
+
+	do {
+		buffer[digits++] = (num % 8) + '0';
+		num = num / 8;
+	} while (num != 0);
+
+	while (digits-- > 0)
+	{
+		if (write(1, &buffer[digits], 1) == -1)
+			return (-1);
+	}
+
+	return (digits + 1);
+}
+
+/**
+ * print_x - Function that prints number in hexadecimal
+ * @num: Number to be printed
+ * Return: Number of digits printed
+ */
+
+int print_x(unsigned int num)
+{
+	int digits = 0;
+	char buffer[10];
+
+	do {
+		buffer[digits++] = "0123456789abcdef"[num % 16];
+		num = num / 16;
+	} while (num != 0);
+
+	while (digits-- > 0)
+	{
+		if (write(1, &buffer[digits], 1) == -1)
+			return (-1);
+	}
+
+	return (digits + 1);
+}
+
+/**
+ * print_X - Function that prints number in hexadecimal in uppercase
+ * @num: Number to be printed
+ * Return: Number of digits printed
+ */
+
+int print_X(unsigned int num)
+{
+	int digits = 0;
+	char buffer[10];
+
+	do {
+		buffer[digits++] = "0123456789ABCDEF"[num % 16];
+		num = num / 16;
+	} while (num != 0);
+
+	while (digits-- > 0)
+	{
+		if (write(1, &buffer[digits], 1) == -1)
+			return (-1);
+	}
+
+	return (digits + 1);
+}
